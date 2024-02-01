91b05d12c58d0122e2ba9eb1a1974befe1678cd9
Khoa Hoang <khoa.hoang@saigontechnology.com>
2024-02-01 14:15:29 +0700

    Updated mesage

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  expect: {
+    timeout: 30 * 1000,
+  },
+  timeout: 300 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
