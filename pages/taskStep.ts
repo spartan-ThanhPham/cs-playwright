4ada51c2189fa441bbe2290ce11808d8361650a3
thanh.phamthi <thanh.phamthi@saigontechnology.com>
2024-02-16 08:28:48 +0000

    Merged in feature/stepout (pull request #18)
    checkout newbranch
    
    * checkout newbranch

--- pages/taskStep.ts
+++ pages/taskStep.ts
@@ -12,21 +12,29 @@ export default class TaskStep {
     }
     //Locators
     txtEmail = () => this.page.locator("//input[@placeholder='you@email.com']");
-    txtPassword = () => this.page.locator ("//input[@name='password']");
-   // btnLogin = () => this.page.getByRole('button', { name: 'Sign in' });
+    txtPassword = () => this.page.locator("//input[@name='password']");
+    // btnLogin = () => this.page.getByRole('button', { name: 'Sign in' });
     btnSignin = () => this.page.locator("//button[text()='Sign in']");
-    linkTaskStep = () => this.page.locator("//*[contains(text(),'Task Step Table')]");
+    linkTaskStep = () => this.page.locator("//*[contains(text(),'Task List/ Work Order Flat View Selector')]");
+    errormesssage = () => this.page.locator("//*[contains(text(),'We couldn’t find an account matching the email and password you enter.')]");
 
     //Sucessfully Login
     public async enterCredential() {
         await this.txtEmail().fill('adminstrator@gmail.com');
         await this.txtPassword().fill('123Pa$$word!');
         await this.btnSignin().click();
-        setTimeout(() => {
-            expect (this.linkTaskStep()).toBeVisible()
-        }, 5000);
-       // await expect(this.page).toHaveURL('https://step-out.maintsys.au/insider/task-step-table');
-    }
-    //expect (this.linkTaskstep()).toBeVisible()
-    ;
-}
+    
+        await expect(this.linkTaskStep()).toBeVisible()
+            }
+
+  //Failed Login
+  public async loginfailed () {
+        await this.txtEmail().fill('adminstrator@gmail.com');
+        await this.txtPassword().fill('123Pa$$word!12');
+        await this.btnSignin().click();
+        await expect(this.errormesssage()).toBeVisible;
+
+        
+    }}
+
+//expect (this.linkTaskstep()).toBeVisible();
--- tests/taskStep.spec.ts
+++ tests/taskStep.spec.ts
@@ -1,10 +1,19 @@
-import {test} from "@playwright/test";
+import { test } from "@playwright/test";
 import TaskStep from "../pages/taskStep";
 
-
-test('Login taskstep', async ({page}) => {
+test('Login taskstep', async ({ page }) => {
 
     const taskStep = new TaskStep(page);
+
+    // //Navigate to login page
     await taskStep.goto();
-    await taskStep.enterCredential();
+    await taskStep.enterCredential()
+})
+
+test('loginfailed',async({ page }) => {
+    const loginfailed= new TaskStep(page);
+    await page.goto ('https://step-out.maintsys.au/auth/signin')
+    await loginfailed.loginfailed();
+  
+
 })
